0ea03b981eaba34f51a654728c490bf0a0614ce2
danlimwz <daniellim@dsaid.gov.sg>
2020-09-23 17:22:39 +0800

    Updates

---
+++ precache-manifest.a78ccdecb05499c58cc41fce4c96625b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b71a1576ede670b791780db1d56082",
+    "url": "/ui-todo-tutorial/index.html"
+  },
+  {
+    "revision": "ed461b7cd91b8f18cf0d",
+    "url": "/ui-todo-tutorial/static/css/main.08700ed6.chunk.css"
+  },
+  {
+    "revision": "b15c11a4df2e16d2aa83",
+    "url": "/ui-todo-tutorial/static/js/2.eef8dbf1.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/ui-todo-tutorial/static/js/2.eef8dbf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed461b7cd91b8f18cf0d",
+    "url": "/ui-todo-tutorial/static/js/main.78d355d0.chunk.js"
+  },
+  {
+    "revision": "ff9ff1abb2c1f2c1bfcf",
+    "url": "/ui-todo-tutorial/static/js/runtime-main.86cf155b.js"
+  }
+]);
